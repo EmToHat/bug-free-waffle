24980ad07e4b140572754ec9f19154827e536bc3
Emilee Hatch <hatch.emilee.cm@gmail.com>
2024-07-18 19:11:36 -0600

    First commit

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyBMOFLGnvgO4bWcbCFFYonKd5AT8riJjkk",
+      authDomain: "oshop-c02e5.firebaseapp.com",
+      projectId: "oshop-c02e5",
+      storageBucket: "oshop-c02e5.appspot.com",
+      messagingSenderId: "373634349949",
+      appId: "1:373634349949:web:8cfb68f74acd742f1eaa14",
+      measurementId: "G-S9GSK1XNKT"
+    }
+  };
